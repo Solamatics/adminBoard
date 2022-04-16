52efe3d494032235573bd7ddf9f9832ec4f3afe2
Olusola Afikode <afikodeolaitan@gmail.com>
2022-04-16 13:37:09 +0100

    complete application sidebar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import "./sidebar.css";
+import LineStyleIcon from "@mui/icons-material/LineStyle";
+import TimelineIcon from "@mui/icons-material/Timeline";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import PersonOutlineRoundedIcon from "@mui/icons-material/PersonOutlineRounded";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
+import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined";
+import BarChartRoundedIcon from "@mui/icons-material/BarChartRounded";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import DynamicFeedOutlinedIcon from "@mui/icons-material/DynamicFeedOutlined";
+import ForumOutlinedIcon from "@mui/icons-material/ForumOutlined";
+import MarkunreadMailboxOutlinedIcon from "@mui/icons-material/MarkunreadMailboxOutlined";
+import ReportTwoToneIcon from "@mui/icons-material/ReportTwoTone";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyleIcon className="sidebarIcon" />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon className="sidebarIcon" />
+              Sales
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <PersonOutlineRoundedIcon className="sidebarIcon" />
+              Users
+            </li>
+            <li className="sidebarListItem">
+              <Inventory2OutlinedIcon className="sidebarIcon" />
+              Products
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoneyOutlinedIcon className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <BarChartRoundedIcon className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <EmailOutlinedIcon className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeedOutlinedIcon className="sidebarIcon" />
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ForumOutlinedIcon className="sidebarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MarkunreadMailboxOutlinedIcon className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <ReportTwoToneIcon className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
