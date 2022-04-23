91586b3b4285d78800cdcfd04ec3d50095ac562e
Olusola Afikode <afikodeolaitan@gmail.com>
2022-04-23 09:46:14 +0100

    built user widget screen

--- src/components/widgetSmall/WidgetSmall.jsx
+++ src/components/widgetSmall/WidgetSmall.jsx
@@ -1,8 +1,90 @@
 import React from "react";
 import "./widgetSmall.css";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 
 const WidgetSmall = () => {
-  return <div className="widgetSmall">WidgetSmall</div>;
+  return (
+    <div className="widgetSmall">
+      <span className="widgetSmallTitle">Newly Added Member</span>
+      <ul className="widgetSmallList">
+        <li className="widgetSmallListItem">
+          <img
+            src="https://images.unsplash.com/photo-1583760919595-270997d7726a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80"
+            alt="avatar"
+            className="widgetSmallImage"
+          />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUsername">Anna Keller</span>
+            <span className="widgetSmallUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmallBtn">
+            <VisibilityOutlinedIcon />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img
+            src="https://images.unsplash.com/photo-1583760919595-270997d7726a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80"
+            alt="avatar"
+            className="widgetSmallImage"
+          />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUsername">Anna Keller</span>
+            <span className="widgetSmallUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmallBtn">
+            <VisibilityOutlinedIcon />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img
+            src="https://images.unsplash.com/photo-1583760919595-270997d7726a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80"
+            alt="avatar"
+            className="widgetSmallImage"
+          />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUsername">Anna Keller</span>
+            <span className="widgetSmallUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmallBtn">
+            <VisibilityOutlinedIcon />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img
+            src="https://images.unsplash.com/photo-1583760919595-270997d7726a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80"
+            alt="avatar"
+            className="widgetSmallImage"
+          />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUsername">Anna Keller</span>
+            <span className="widgetSmallUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmallBtn">
+            <VisibilityOutlinedIcon />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img
+            src="https://images.unsplash.com/photo-1583760919595-270997d7726a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80"
+            alt="avatar"
+            className="widgetSmallImage"
+          />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUsername">Anna Keller</span>
+            <span className="widgetSmallUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmallBtn">
+            <VisibilityOutlinedIcon />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default WidgetSmall;
