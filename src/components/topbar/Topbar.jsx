16bbc9e45e9e4663cbe0cc85a91cabb3897adba3
Olusola Afikode <afikodeolaitan@gmail.com>
2022-04-24 18:11:15 +0100

    complete homepage

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -27,7 +27,7 @@ const Topbar = () => {
           </div>
           <img
             src="https://images.unsplash.com/photo-1583760919595-270997d7726a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80"
-            alt="avatarImage"
+            alt="avatar"
             className="topAvatar"
           />
         </div>
--- src/components/widgetLarge/WidgetLarge.jsx
+++ src/components/widgetLarge/WidgetLarge.jsx
@@ -2,7 +2,83 @@ import React from "react";
 import "./widgetLarge.css";
 
 const WidgetLarge = () => {
-  return <div className="widgetLarge">WidgetLarge</div>;
+  const Button = ({ type }) => {
+    return <button className={"widgetLargeBtn " + type}>{type}</button>;
+  };
+
+  return (
+    <div className="widgetLarge">
+      <h3 className="widgetLargeTitle">Latest Transactions</h3>
+      <table className="widgetLargeTable">
+        <tr className="widgetLargeTr">
+          <th className="widgetLargeTh">Customer</th>
+          <th className="widgetLargeTh">Date</th>
+          <th className="widgetLargeTh">Amount</th>
+          <th className="widgetLargeTh">Status</th>
+        </tr>
+        <tr className="widgetLargeTr">
+          <td className="widgetLargeUser">
+            <img
+              src="https://images.unsplash.com/photo-1583760919595-270997d7726a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80"
+              alt="user"
+              className="widgetLargeImg"
+            />
+            <span className="widgetLargeName">Susan Carol</span>
+          </td>
+          <td className="widgetLargeDate">2 Jun 2021</td>
+          <td className="widgetLargeAmount">$122.00</td>
+          <td className="widgetLargeStatus">
+            <Button type="Approved"></Button>
+          </td>
+        </tr>
+        <tr className="widgetLargeTr">
+          <td className="widgetLargeUser">
+            <img
+              src="https://images.unsplash.com/photo-1583760919595-270997d7726a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80"
+              alt="user"
+              className="widgetLargeImg"
+            />
+            <span className="widgetLargeName">Susan Carol</span>
+          </td>
+          <td className="widgetLargeDate">2 Jun 2021</td>
+          <td className="widgetLargeAmount">$122.00</td>
+          <td className="widgetLargeStatus">
+            <Button type="Declined"></Button>
+          </td>
+        </tr>
+        <tr className="widgetLargeTr">
+          <td className="widgetLargeUser">
+            <img
+              src="https://images.unsplash.com/photo-1583760919595-270997d7726a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80"
+              alt="user"
+              className="widgetLargeImg"
+            />
+            <span className="widgetLargeName">Susan Carol</span>
+          </td>
+          <td className="widgetLargeDate">2 Jun 2021</td>
+          <td className="widgetLargeAmount">$122.00</td>
+          <td className="widgetLargeStatus">
+            <Button type="Pending"></Button>
+          </td>
+        </tr>
+        <tr className="widgetLargeTr">
+          <td className="widgetLargeUser">
+            <img
+              src="https://images.unsplash.com/photo-1583760919595-270997d7726a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80"
+              alt="user"
+              className="widgetLargeImg"
+            />
+            <span className="widgetLargeName">Susan Carol</span>
+          </td>
+          <td className="widgetLargeDate">2 Jun 2021</td>
+          <td className="widgetLargeAmount">$122.00</td>
+          <td className="widgetLargeStatus">
+            <Button type="Approved"></Button>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default WidgetLarge;
