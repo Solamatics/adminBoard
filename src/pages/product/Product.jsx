a720fd899e02ab10504f45d94e76c1915bb49400
Olusola Afikode <afikodeolaitan@gmail.com>
2022-05-04 05:15:11 +0100

    buidling single product page

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./product.css";
+import { Link } from "react-router-dom";
+import Chart from "../../components/chart/Chart";
 
 const Product = () => {
-  return <div className="product">Product</div>;
+  return (
+    <div className="product">
+      <div className="productTitleContainer">
+        <h1 className="productTitle">Product</h1>
+        <Link to="/newproduct">
+          <button className="productAddButton">Create</button>
+        </Link>
+      </div>
+      <div className="productTop">
+        <div className="productTopLeft">
+          <Chart />
+        </div>
+        <div className="productTopRight"></div>
+      </div>
+      <div className="productBottom"></div>
+    </div>
+  );
 };
 
 export default Product;
