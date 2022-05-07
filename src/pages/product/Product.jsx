d7f2aa4b04ecf29953aaeeafcbc37963cf2a657c
Olusola Afikode <afikodeolaitan@gmail.com>
2022-05-07 06:17:41 +0100

    complete building product list page

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -3,6 +3,7 @@ import "./product.css";
 import { Link } from "react-router-dom";
 import Chart from "../../components/chart/Chart";
 import { productData } from "../../data";
+import PublishIcon from "@mui/icons-material/Publish";
 
 const Product = () => {
   return (
@@ -33,23 +34,54 @@ const Product = () => {
             </div>
 
             <div className="productInfoItem">
-              <span className="productInfoKey">id:</span>
-              <span className="productInfoValue">123</span>
+              <span className="productInfoKey">sales:</span>
+              <span className="productInfoValue">5123</span>
             </div>
 
             <div className="productInfoItem">
-              <span className="productInfoKey">id:</span>
-              <span className="productInfoValue">123</span>
+              <span className="productInfoKey">active:</span>
+              <span className="productInfoValue">yes</span>
             </div>
 
             <div className="productInfoItem">
-              <span className="productInfoKey">id:</span>
-              <span className="productInfoValue">123</span>
+              <span className="productInfoKey">in stock:</span>
+              <span className="productInfoValue">no</span>
             </div>
           </div>
         </div>
       </div>
-      <div className="productBottom"></div>
+      <div className="productBottom">
+        <form className="productForm">
+          <div className="productFormLeft">
+            <label>Product Name</label>
+            <input type="text" placeholder="Apple Airpod" />
+            <label>In Stock</label>
+            <select name="inStock" id="idStock">
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+            <label>Active</label>
+            <select name="active" id="active">
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+          </div>
+          <div className="productFormRight">
+            <div className="productUpload">
+              <img
+                src="https://images.pexels.com/photos/5099868/pexels-photo-5099868.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="productImage"
+                className="productUploadImg"
+              />
+              <label for="file">
+                <PublishIcon />
+              </label>
+              <input type="file" id="file" style={{ display: "none" }} />
+            </div>
+            <button className="productButton">Update</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
